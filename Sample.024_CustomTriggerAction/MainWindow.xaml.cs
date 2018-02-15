5e6aff23a5507bbca208cc7387369a24e4a71d7a
Charles Kwon <youngcheol.kwon@gmail.com>
2018-02-16 01:08:05 +0900

    024 샘플

---
+++ Sample.024_CustomTriggerAction/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Sample._024_CustomTriggerAction
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            txtHour.Text = DateTime.Now.Hour.ToString();
+            txtMinute.Text = DateTime.Now.Minute.ToString();
+            txtSecond.Text = DateTime.Now.Second.ToString();
+        }
+    }
+}
---
+++ Sample.024_CustomTriggerAction/TimeDecorateBehavior.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace Sample.Behavior
+{
+    public class TextChangedTrigger : TriggerBase<TextBlock>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.Loaded += AssociatedObject_Loaded;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+        }
+
+        private void AssociatedObject_Loaded(object sender, RoutedEventArgs e)
+        {
+            var textBlock = AssociatedObject as TextBlock;
+            if (textBlock == null) return;
+
+            var dpd = DependencyPropertyDescriptor.FromProperty(TextBlock.TextProperty, typeof(TextBlock));
+            if (dpd == null) return;
+            dpd.AddValueChanged(textBlock, OnTextChanged);
+        }
+
+        private void OnTextChanged(object sender, EventArgs e)
+        {
+            InvokeActions(sender);
+        }
+    }
+
+    public class AddZeroAction : TriggerAction<TextBlock>
+    {
+        protected override void Invoke(object parameter)
+        {
+            var textBlock = parameter as TextBlock;
+            if (textBlock == null) return;
+            if (textBlock.Text.Length == 1)
+            {
+                textBlock.Text = "0" + textBlock.Text;
+            }
+        }
+    }
+}
