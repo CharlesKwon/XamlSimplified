1babe0a06205e73b3d14ade069ce5798df9b6a05
Charles Kwon <youngcheol.kwon@gmail.com>
2018-03-25 21:53:44 +0900

    슬라이더 커스텀 팝업 예제 태스트

---
+++ Sample.032_SliderWithFollowingPopup/ExPopup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Controls.Primitives;
+
+namespace Sample.Control
+{
+    public class FollowingPopup : Popup
+    {
+        protected override void OnOpened(EventArgs e)
+        {
+            base.OnOpened(e);
+
+            this.PlacementTarget.PreviewMouseMove += PlacementTarget_PreviewMouseMove;
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            this.PlacementTarget.PreviewMouseMove -= PlacementTarget_PreviewMouseMove;
+
+            base.OnClosed(e);
+        }
+
+        private void PlacementTarget_PreviewMouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            this.HorizontalOffset += +0.1;
+            this.HorizontalOffset += -0.1;
+        }
+    }
+}
