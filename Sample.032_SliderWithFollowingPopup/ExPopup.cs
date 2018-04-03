e6796ef50235b30a6a73503fed24648e83ad9494
Charles Kwon <youngcheol.kwon@gmail.com>
2018-04-03 23:25:42 +0900

    슬라이더 팝업 새로운 비헤이비어 샘플 완료

--- Sample.032_SliderWithFollowingPopup/ExPopup.cs
+++ Sample.032_SliderWithFollowingPopup/ExPopup.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Controls.Primitives;
+using System.Windows.Media;
 
 namespace Sample.Control
 {
@@ -24,5 +26,29 @@ namespace Sample.Control
             this.HorizontalOffset += +0.1;
             this.HorizontalOffset += -0.1;
         }
+
+        #region UI Helper < Common 
+
+        public static T FindParent<T>(DependencyObject child) where T : DependencyObject
+        {
+            //get parent item
+            DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+
+            //we've reached the end of the tree
+            if (parentObject == null) return null;
+
+            //check if the parent matches the type we're looking for
+            T parent = parentObject as T;
+            if (parent != null)
+            {
+                return parent;
+            }
+            else
+            {
+                return FindParent<T>(parentObject);
+            }
+        }
+
+        #endregion
     }
 }
---
+++ Sample.032_SliderWithFollowingPopup/ToolTipToPopupSliderBehavior.cs
@@ -0,0 +1,161 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+using System.Windows.Media;
+
+namespace Sample.Behavior
+{
+    public class ToolTipToPopupSliderBehavior : Behavior<Slider>
+    {
+        #region Variable
+
+        private Thumb _thumb;
+        private Popup _popup;
+
+        #endregion
+
+        #region Proteced method 
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.Loaded += AssociatedObject_Loaded;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.Loaded -= AssociatedObject_Loaded;
+            //_thumb.QueryCursor -= _thumb_QueryCursor;
+            //_thumb.MouseMove -= _thumb_MouseMove;
+            _thumb.MouseLeave -= _thumb_MouseLeave;
+            AssociatedObject.PreviewKeyUp -= Slider_PreviewKeyUp;
+
+            _thumb = null;
+            _popup = null;
+
+            base.OnDetaching();
+        }
+
+        #endregion
+
+        #region Event handler 
+
+        private void AssociatedObject_Loaded(object sender, RoutedEventArgs e)
+        {
+            _popup = GetVisualChild<Popup>(AssociatedObject);
+            _thumb = GetVisualChild<Thumb>(AssociatedObject);
+            //_popup = GetVisualChildByName<Popup>(AssociatedObject, "PART_ToolTipPopup");
+            //_thumb = GetVisualChildByName<Thumb>(AssociatedObject, "Thumb");
+            if (_thumb == null || _popup == null) return;
+
+            //_thumb.QueryCursor += _thumb_QueryCursor;
+            //_thumb.MouseMove += _thumb_MouseMove;
+            _thumb.MouseLeave += _thumb_MouseLeave;
+            AssociatedObject.PreviewKeyUp += Slider_PreviewKeyUp;
+
+            AssociatedObject.ValueChanged += AssociatedObject_ValueChanged;
+        }
+
+        private void AssociatedObject_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            _popup.HorizontalOffset = 0;
+            _popup.IsOpen = true;
+
+            var xValRatio = AssociatedObject.ActualWidth / (AssociatedObject.Maximum - AssociatedObject.Minimum);
+            if (e.NewValue > e.OldValue && e.NewValue != AssociatedObject.Maximum)
+            {
+                _popup.HorizontalOffset += xValRatio;
+            }
+            else if (e.NewValue < e.OldValue && e.OldValue != AssociatedObject.Maximum)
+            {
+                _popup.HorizontalOffset -= xValRatio;
+            }
+        }
+
+        #region Event handler < Mouse
+
+        //private void _thumb_QueryCursor(object sender, QueryCursorEventArgs e)
+        //{
+        //    _popup.IsOpen = true;        
+        //}
+
+        //private void _thumb_MouseMove(object sender, MouseEventArgs e)
+        //{
+        //    _popup.IsOpen = true;
+        //}
+
+        private void _thumb_MouseLeave(object sender, MouseEventArgs e)
+        {
+            _popup.IsOpen = false;
+        }
+
+        #endregion
+
+        #region Event handler < Keyboard
+                
+        private void Slider_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            _popup.IsOpen = false;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region UI Helper < Common 
+
+        public static T GetVisualChild<T>(DependencyObject parent) where T : Visual
+        {
+            if (parent == null) return null;
+
+            T child = default(T);
+
+            int numVisuals = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < numVisuals; i++)
+            {
+                Visual v = (Visual)VisualTreeHelper.GetChild(parent, i);
+                child = v as T;
+                if (child == null)
+                {
+                    child = GetVisualChild<T>(v);
+                }
+                if (child != null)
+                {
+                    break;
+                }
+            }
+
+            return child;
+        }
+
+        public static T GetVisualChildByName<T>(DependencyObject parent, string childName) where T : Visual
+        {
+            if (parent == null) return null;
+
+            T child = default(T);
+
+            int numVisuals = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < numVisuals; i++)
+            {
+                Visual v = (Visual)VisualTreeHelper.GetChild(parent, i);
+                child = v as T;
+                if (child == null)
+                {
+                    child = GetVisualChildByName<T>(v, childName);
+                }
+                if (child != null)
+                {
+                    break;
+                }
+            }
+
+            return child;
+        }
+
+        #endregion
+    }
+}
